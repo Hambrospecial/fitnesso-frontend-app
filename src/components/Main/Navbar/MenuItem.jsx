feb5932a74e3b24721a2606bb096afb293669d26
Francis Oyiogu <francis.oyiogu@decagon.dev>
2022-02-16 12:10:38 +0100

    Revert "A feat home page d"

---
+++ src/components/Main/Navbar/MenuItem.jsx
@@ -0,0 +1,27 @@
+export const MenuItem = [
+    {
+        title: "Home",
+        url: "#",
+        cName: "nav-links"
+    },
+
+    {
+        title: "Product",
+        url: "#",
+        cName: "nav-links"
+    },
+
+    {
+        title: "Contact Us",
+        url: "#",
+        cName: "nav-links"
+    },
+
+    {
+        title: "Log In",
+        url: "#",
+        cName: "nav-link"
+    },
+
+  
+]
